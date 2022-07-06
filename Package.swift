d10d907bc77404a555185a24eccbce9e54aed602
kotovmax <kotov.md@yandex.ru>
2022-07-06 18:50:53 +0300

    Fixed archive links

--- Package.swift
+++ Package.swift
@@ -38,22 +38,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/kotovmax/googlemapsspm/releases/download/v7.0.0/GoogleMaps.xcframework.zip",
+            url: "https://github.com/kotovmax/googlemapsspm/releases/download/7.0.0/GoogleMaps.xcframework.zip",
             checksum: "fedd67e86116a5c56002432f8a280b8252c10ae8f0ec79cc34e56fcb5fac7590"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/kotovmax/googlemapsspm/releases/download/v7.0.0/GoogleMapsBase.xcframework.zip",
+            url: "https://github.com/kotovmax/googlemapsspm/releases/download/7.0.0/GoogleMapsBase.xcframework.zip",
             checksum: "9e50b591c317f93deb4a302c3c52e873cbc7c0e7fa38e8af54ccd904e9fd31c2"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/kotovmax/googlemapsspm/releases/download/v7.0.0/GoogleMapsCore.xcframework.zip",
+            url: "https://github.com/kotovmax/googlemapsspm/releases/download/7.0.0/GoogleMapsCore.xcframework.zip",
             checksum: "2baca4bb28dde781d0f729775d79b89092ab364a1b708e3f7b0c23a823611955"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/kotovmax/googlemapsspm/releases/download/v7.0.0/GoogleMapsM4B.xcframework.zip",
+            url: "https://github.com/kotovmax/googlemapsspm/releases/download/7.0.0/GoogleMapsM4B.xcframework.zip",
             checksum: "20667b3195cd31ff84d47a308f0a577ab0afb2b2b72ae70a9e55a6e26aae10a4"
         ),
     ]
--- README.md
+++ README.md
@@ -4,14 +4,14 @@ SPM support for GoogleMaps
 How to update:
 1) Find archive link for desirable version of GoogleMaps at [CocoaPods repo](https://github.com/CocoaPods/Specs/tree/master/Specs/a/d/d/GoogleMaps)
 2) Download archive
-3) Extract archive and compress to again to `zip`
-4) Compute checksum for archive
+3) Extract archive and compress frameworks into separate `zip` archives
+4) Compute checksum for archives
 ```sh
 swift package compute-checksum ../path/to/archive.zip
 ```
-5) Update Package.swift with archive link from future GitHub Release and checksum
+5) Update Package.swift with archive links from future GitHub Release and checksums
 6) Commit and push changes
-7) Create GitHub Release, add `zip` archive as binary assests
+7) Create GitHub Release, add `zip` archives as binary assests
 
 
 
